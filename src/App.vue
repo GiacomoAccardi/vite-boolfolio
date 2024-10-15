d5bd96f45bf9d92952248dd08d4945b63fa8b3bd
giacomohrd <giacomo.accardi.lavoro@gmail.com>
2024-10-15 18:30:30 +0200

    [FIX]

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,50 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
+<script>
+import axios from 'axios';
+
+export default {
+  data(){
+    return {
+      projects: []
+    }
+  },
+  methods:{
+    getAllProjects(){
+      axios.get('http://127.0.0.1:8000/api/projects').then((res) => {
+       this.projects = res.data.results;
+      });
+    }
+  },
+  created(){
+    this.getAllProjects();
+  }
+}
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <div class="container">
+    <div class="row">
+      <div class="col-12">
+        <h1 class="text-center">Elenco progetti</h1>
+      </div>
+      <div class="col-12">
+
+        <div class="row gy-4">
+          <div class="col-12 col-md-5 col-lg-4" v-for="proj in projects" :key="proj.id">
+            <div class="card">
+              <img :src="proj.image != null ? `http://127.0.0.1:8000/storage/${proj.image}` : 'https://coffective.com/wp-content/uploads/2018/06/default-featured-image.png.jpg'" alt="" class="card-img-top">
+              <div class="card-body">
+                <h4 class="card-title">{{proj.name}}</h4>
+                <p>{{proj.summary}}</p>
+              </div>
+            </div>
+          </div>
+        </div>
+
+      </div>
+    </div>
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
+<style lang="scss">
+  @import './styles/generals.scss'
 </style>
